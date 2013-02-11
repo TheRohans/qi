2d87b7dcfd490d187e4604266f30719b5616ed07
Rob Rohan <robrohan@gmail.com>
2013-02-11 01:51:43 -0800

    messing with styles a bit, adding reference to what works

--- src/qestyles.h
+++ src/qestyles.h
@@ -1,3 +1,22 @@
+/*
+Terminal Color	RGB Value Used by SGD
+Black	        0 0 0
+Light red	    255 0 0
+Light green	    0 255 0
+Yellow	        255 255 0
+Light blue	    0 0 255
+Light magenta	255 0 255
+Light cyan	    0 255 255
+High white	    255 255 255
+Gray	        128 128 128
+Red	            128 0 0
+Green	        0 128 0
+Brown	        128 128 0
+Blue	        0 0 128
+Magenta	        128 0 128
+Cyan	        0 128 128
+White	        192 192 192
+*/
     /* root style, must be complete */
     STYLE_DEF(QE_STYLE_DEFAULT, "default",
               QERGB(0xf8, 0xd8, 0xb0), QERGB(0x00, 0x00, 0x00),
@@ -24,35 +43,35 @@
 
     /* coloring styles */
     STYLE_DEF(QE_STYLE_HIGHLIGHT, "highlight",
-              QERGB(0x00, 0x00, 0x00), QERGB(0xf8, 0xd8, 0xb0),
+              QERGB(0x00, 0x00, 0x00), QERGB(0xff, 0xcc, 0x99),
               0, 0)
     STYLE_DEF(QE_STYLE_SELECTION, "selection",
               QERGB(0xff, 0xff, 0xff), QERGB(0x00, 0x00, 0xff),
               0, 0)
     STYLE_DEF(QE_STYLE_COMMENT, "comment",
-              QERGB(0xf8, 0x44, 0x00), COLOR_TRANSPARENT,
+              QERGB(0xff, 0x00, 0x00), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_PREPROCESS, "preprocess",
-              QERGB(0x00, 0xff, 0xff), COLOR_TRANSPARENT,
+              QERGB(0x00, 0x00, 0xff), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_STRING, "string",
-              QERGB(0xf8, 0xa0, 0x78), COLOR_TRANSPARENT,
+              QERGB(0x00, 0xff, 0x00), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_KEYWORD, "keyword",
               QERGB(0x00, 0xff, 0xff), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_FUNCTION, "function",
-              QERGB(0x80, 0xcc, 0xf0), COLOR_TRANSPARENT,
+              QERGB(0xff, 0xff, 0xff), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_VARIABLE, "variable",
-              QERGB(0xe8, 0xdc, 0x80), COLOR_TRANSPARENT,
+              QERGB(0xc0, 0xc0, 0xc0), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_TYPE, "type",
-              QERGB(0x98, 0xf8, 0x98), QERGB(0x00, 0x00, 0xff), //COLOR_TRANSPARENT,
+              QERGB(0x80, 0x80, 0x00), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_TAG, "tag",
-              QERGB(0x00, 0xff, 0xff), COLOR_TRANSPARENT,
+              QERGB(0x00, 0xff, 0x00), COLOR_TRANSPARENT,
               0, 0)
     STYLE_DEF(QE_STYLE_CSS, "css",
-              QERGB(0x98, 0xf8, 0x98), COLOR_TRANSPARENT,
+              QERGB(0x00, 0xff, 0xff), COLOR_TRANSPARENT,
               0, 0)
