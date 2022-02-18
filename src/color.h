5eb33c2613e591c1edeb459b9ddc3b5988aafcfe
Rob Rohan <rob@therohans.com>
2022-02-18 21:33:03 +1300

    Space

--- src/color.h
+++ src/color.h
@@ -2,8 +2,10 @@
 #define QE_COLOR_H
 
 typedef unsigned int QEColor;
+
 #define QEARGB(a,r,g,b) (((a) << 24) | ((r) << 16) | ((g) << 8) | (b))
 #define QERGB(r,g,b) QEARGB(0xff, r, g, b)
+
 #define COLOR_TRANSPARENT 0
 #define QECOLOR_XOR       1
 
